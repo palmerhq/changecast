89e88be5ed3e46cf179065be63c6f82016843580
jack <jackjocross@gmail.com>
2019-02-08 17:47:07 -0600

    Remove gatsby-source-filesystem from config

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,13 +5,6 @@ config()
 module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        name: 'data',
-        path: `${__dirname}/data`,
-      },
-    },
     'gatsby-transformer-json',
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
