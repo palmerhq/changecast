9ba62c8f63a247acdc40d4688a5c0eeb7265e52b
jack <jackjocross@gmail.com>
2019-03-05 22:19:35 -0500

    Attempt full build command

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -1,3 +1,16 @@
+const path = require('path')
+const hexRgb = require('hex-rgb')
+const { config } = require('dotenv')
+
+config({ path: path.resolve('..', '.env') })
+
 module.exports = {
+  siteMetadata: {
+    exampleSiteUrls: [
+      process.env.FIRST_EXAMPLE_URL,
+      process.env.SECOND_EXAMPLE_URL,
+      process.env.THIRD_EXAMPLE_URL,
+    ]
+  },
   plugins: ['gatsby-plugin-emotion', 'gatsby-plugin-react-helmet'],
 }
