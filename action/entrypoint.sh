3f23cdc9f8f978704ab3bf03c325770e3c4f5ebb
jack <jackjocross@gmail.com>
2019-04-19 19:03:55 -0400

    Fix entrypoint

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -2,10 +2,7 @@
 
 cd /changecast
 
-$* \
-GITHUB_REPO_URL="https://github.com/$GITHUB_REPOSITORY" \
-GITHUB_ACCESS_TOKEN=$GITHUB_TOKEN \
-yarn build
+sh -c "$* GITHUB_REPO_URL=https://github.com/$GITHUB_REPOSITORY GITHUB_ACCESS_TOKEN=$GITHUB_TOKEN yarn build"
 
 mkdir "$GITHUB_WORKSPACE/changecast"
 cp -r /changecast/site/public/. "$GITHUB_WORKSPACE/changecast"
