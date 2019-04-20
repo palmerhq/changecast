9731e4ffa4451a839d4c644e430189c881ca7594
jack <jackjocross@gmail.com>
2019-04-19 20:40:21 -0400

    Update pr action

--- action/pr/entrypoint.sh
+++ action/pr/entrypoint.sh
@@ -1,9 +1,5 @@
 #!/bin/sh -l
 
-cd /changecast
-
-git checkout "$GITHUB_SHA"
-
 yarn install
 
 sh -c "GITHUB_REPO_URL=https://github.com/$GITHUB_REPOSITORY GITHUB_ACCESS_TOKEN=$GITHUB_TOKEN yarn build"
