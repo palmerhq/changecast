3305641d6eceb8de0ea818b52dec3b5db5c6fa43
jack <jackjocross@gmail.com>
2019-04-19 21:17:33 -0400

    Allow for REPO_URL overwrite

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -2,7 +2,11 @@
 
 cd /changecast
 
-sh -c "GITHUB_REPO_URL=https://github.com/$GITHUB_REPOSITORY GITHUB_ACCESS_TOKEN=$GITHUB_TOKEN yarn build"
+if [[ -z "${REPO_URL}" ]]; then
+  sh -c "GITHUB_REPO_URL=$REPO_URL GITHUB_ACCESS_TOKEN=$GITHUB_TOKEN yarn build"
+else
+  sh -c "GITHUB_REPO_URL=https://github.com/$GITHUB_REPOSITORY GITHUB_ACCESS_TOKEN=$GITHUB_TOKEN yarn build"
+fi
 
 mkdir "$GITHUB_WORKSPACE/changecast"
 cp -r /changecast/site/public/. "$GITHUB_WORKSPACE/changecast"
